c38a4f111eefc3ead9e83f769b974d1239d6501a
ym4199 <ym4199@gmail.com>
2017-06-30 01:28:50 +0900

    modify alert.py

--- lesson4/alert.py
+++ lesson4/alert.py
@@ -1,6 +1,24 @@
+import urllib
+
+
 def read_text():
     quotes = open()
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
+
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+    if "true" in output:
+        print("Profanity Alert!!")
+    elif "false" in output:
+        print("This document has no curse words!")
+    else:
+        print("Could not scan the document properly.")
+
 read_text()
